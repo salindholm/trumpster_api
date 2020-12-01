97163195d2f8c693005c8b90a5d895ae3530c6f2
Sanne <sannelindholm@hotmail.com>
2020-12-01 16:37:31 +0100

    Removing parenthesis in rails_helper.rb and adding ../ to file path. Rspec working.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../config/environment', __FILE__)
+require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 begin
@@ -18,5 +18,5 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
   config.include FactoryBot::Syntax::Methods
-  config.include(Shoulda::Matchers::ActiveRecord, type: :model)
+  config.include Shoulda::Matchers::ActiveRecord, type: :model
 end
