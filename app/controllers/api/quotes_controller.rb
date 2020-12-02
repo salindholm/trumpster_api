466bce474984afd64ef029c810cb526995e9a5a4
swardbladpaulo <paulo@swardblads.se>
2020-12-02 20:49:51 +0100

    trobleshooting

---
+++ app/controllers/api/quotes_controller.rb
@@ -0,0 +1,22 @@
+class Api::QuotesController < ApplicationController
+  API_URL = 'https://api.tronalddump.io'.freeze
+
+  def show
+    if params[:id] == 'random'
+      url = "#{API_URL}/random/quote"
+      result = RestClient.get(url)
+    end
+    quote = JSON.parse(result.body)
+    render json: format_json_response(quote)
+  end
+
+  private
+
+  def format_json_response(obj)
+    {
+      date: DateTime.parse(obj['appeared_at']).strftime('%B %Y'),
+      source: obj['_embedded']['source'][0]['url'],
+      random_quote: obj['value']
+    }
+  end
+end
