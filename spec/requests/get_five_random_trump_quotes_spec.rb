ef8f7575bf3a27e2b531df1370ef582fb204e960
Kyungin Na <nakyungin93@gmail.com>
2020-12-02 19:50:58 +0100

    Resolving conflict

--- spec/requests/get_five_random_trump_quotes_spec.rb
+++ spec/requests/get_five_random_trump_quotes_spec.rb
@@ -12,6 +12,9 @@ RSpec.describe 'GET /quotes/:id', type: :request do
         "source": 'https://twitter.com/realDonaldTrump/status/787699930718695425',
         "quote_of_the_day": 'The election is absolutely being rigged by the dishonest and distorted media pushing Crooked Hillary - but also at many polling places - SAD'
       }.as_json
+      
+      binding.pry
+      
 
       expect(JSON.parse(response.body)).to eq expected_response_body
     end
