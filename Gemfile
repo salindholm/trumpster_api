7a9fa8e5a81550d5c2d79bc9c44334193830e90e
Kyungin Na <nakyungin93@gmail.com>
2020-12-02 20:44:51 +0100

    Adding rack-cors in Gemfile and bundle install.

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'pg', '>= 0.18', '< 2.0'
 gem 'puma', '~> 4.1'
 gem 'bootsnap', '>= 1.4.2', require: false
 gem 'rest-client'
+gem 'rack-cors', require: 'rack/cors'
 
 group :development, :test do
   gem 'rspec-rails'
--- app/controllers/api/quotes_controller.rb
+++ app/controllers/api/quotes_controller.rb
@@ -1,10 +1,5 @@
-<<<<<<< HEAD:app/controllers/quotes_controller.rb
-class QuotesController < ApplicationController
-  API_URL = "https://api.tronalddump.io"
-=======
 class Api::QuotesController < ApplicationController
-  API_URL = 'https://api.tronalddump.io'.freeze
->>>>>>> c31a4dc7d6f665b065eefc2db6fc706530debba0:app/controllers/api/quotes_controller.rb
+  API_URL = 'https://api.tronalddump.io'
 
   def show
     if params[:id] == "random"
@@ -18,15 +13,9 @@ class Api::QuotesController < ApplicationController
 
   def format_json_response(obj)
     {
-<<<<<<< HEAD:app/controllers/quotes_controller.rb
-      date: DateTime.parse(obj["appeared_at"]).strftime("%B %Y"),
-      source: obj["_embedded"]["source"][0]["url"],
-      quote_of_the_day: obj["value"],
-=======
       date: DateTime.parse(obj['appeared_at']).strftime('%B %Y'),
       source: obj['_embedded']['source'][0]['url'],
       random_quote: obj['value']
->>>>>>> c31a4dc7d6f665b065eefc2db6fc706530debba0:app/controllers/api/quotes_controller.rb
     }
   end
 end
