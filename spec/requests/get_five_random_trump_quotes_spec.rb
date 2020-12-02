dc48a10526a9f15b12529bc62baa6dc5430a1219
Sanne <sannelindholm@hotmail.com>
2020-12-02 10:23:18 +0100

    Adding date, source and quote to expected response body and transforming it to json format in test. Also removing binding.pry

--- spec/requests/get_five_random_trump_quotes_spec.rb
+++ spec/requests/get_five_random_trump_quotes_spec.rb
@@ -1,25 +1,19 @@
 # frozen_string_literal: true
 
-RSpec.describe "GET /quotes/:id", type: :request do
+RSpec.describe 'GET /quotes/:id', type: :request do
   describe 'with "random" as query parameters' do
     before do
-      get "/quotes/random"
+      get '/quotes/random'
     end
 
-    it "is expected to return a quote" do
+    it 'is expected to return a quote' do
       expected_response_body = {
-        "date": '',
-        "source": '',
-        "quote_of_the_day": ''
-      }
-      
+        "date": 'October 2016',
+        "source": 'https://twitter.com/realDonaldTrump/status/787699930718695425',
+        "quote_of_the_day": 'The election is absolutely being rigged by the dishonest and distorted media pushing Crooked Hillary - but also at many polling places - SAD'
+      }.as_json
+
       expect(JSON.parse(response.body)).to eq expected_response_body
-    end
-
-    it "is expected to..." do
-    end
-
-    it "is expected to..." do
     end
   end
 end
